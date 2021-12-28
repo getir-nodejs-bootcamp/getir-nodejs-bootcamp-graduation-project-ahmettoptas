081cbdd116b4e376aa3601512020e99b3a20d139
ahmettoptas <ahmetktoptas97@gmail.com>
2021-12-28 05:59:47 +0300

    port is changed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ connection();
 
 
 //Server accepts only /records url, otherwise it directs to error
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000...");
     app.use("/records",recordRoutes);
     app.use("/*",errorRoutes);
